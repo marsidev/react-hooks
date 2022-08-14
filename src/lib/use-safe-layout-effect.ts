dd639677e42f67932de3af094e5f0fff03ce3cd6
Luis Marsiglia <marsiglia.business@gmail.com>
2022-08-13 23:51:19 -0400

    fix: prevent path alias

--- src/lib/use-safe-layout-effect.ts
+++ src/lib/use-safe-layout-effect.ts
@@ -1,5 +1,5 @@
 import { useEffect, useLayoutEffect } from 'react'
-import { isBrowser } from '~/utils/dom'
+import { isBrowser } from '../utils/dom'
 
 /**
  * Uses useLayoutEffect if the environment is a browser, otherwise uses useEffect.
